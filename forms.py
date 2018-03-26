0cefdf8ec99780c2e8374a9d69269f6139d05000
Josh <howellj0122@gmail.com>
2018-03-25 21:14:34 -0400

    Login/logout

--- forms.py
+++ forms.py
@@ -8,3 +8,8 @@ class SignupForm(Form):
 	email = StringField('Email', validators=[DataRequired("I won't spam you. Nor do I care to."), Email("Tsk, tsk...This is not valid!")])
 	password = PasswordField('Password', validators=[DataRequired("I won't know your pw...or will I? ;)"), Length(min=6, message="Make it more than 6 characters fool!")])
 	submit = SubmitField('Sign up')
+
+class LoginForm(Form):
+	email = StringField('Email', validators=[DataRequired("Enter your email bruh!"), Email("Email bro!!")])
+	password = PasswordField('Password', validators=[DataRequired("We need that password bro!")])
+	submit = SubmitField("Sign in")
